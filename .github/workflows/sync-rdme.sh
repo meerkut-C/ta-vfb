55d3e85de0f54c5afb25f0c6814264010d93df5f
Meerk <meerkut8@gmail.com>
2021-11-05 17:29:50 +0200

    literal to symbolic

--- .github/workflows/sync-rdme.sh
+++ .github/workflows/sync-rdme.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
+DIR='mark-viewport'
+FNa='README.md'
+FNb='LICENSE'
 FN_log='.generated.log'
+
 : > "${FN_log}"
 exec 5<> "${FN_log}"
 echo 'sync-rdme.sh starting...' >&5
 echo -n 'pwd=' >&5 ; pwd >&5
-
-FNa='README.md'
-FNb='LICENSE'
-DIR='mark-viewport'
-
 DIROK=$(find . -maxdepth 1 -type d -name "${DIR}" | wc -l)
 if [ x"${DIROK}" = x"1" ]
 then
@@ -18,7 +17,7 @@ then
   then
     echo 'no extra commit, files match' >&5
   else
-    cp "${FNa}" "${FNb}" ./mark-viewport/             >&5
+    cp "${FNa}" "${FNb}"  './'"${DIR}"'/'             >&5
     git config user.name meerkut-C                    >&5
     git add .                                         >&5
     git commit -m "an extra commit from sync-rdme.sh" >&5
@@ -26,6 +25,6 @@ then
     echo '+ extra commit produced'                    >&5
   fi
 else
-  echo 'there will be no additional commit' >&5
+  echo 'there will be no additional commit, missing target dir' >&5
 fi
 echo '  done.'  >&5
